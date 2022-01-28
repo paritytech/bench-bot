e36769879bf08b5dedba393ed91167a077193b6f
João Paulo Silva de Souza <77391175+joao-paulo-parity@users.noreply.github.com>
2022-01-28 15:29:40 -0300

    Only restart the app after the current event finishes (#85)
    * move mutex to event handlers
    
    so that we only restart the app after the current event finishes
    
    * adjust text
    
    * clarify comment
    
    * use isWaitingForExitingWhenFree for tracking when the application should exit

--- bench.js
+++ bench.js
@@ -8,8 +8,6 @@ function errorResult(message, error) {
 
 let cwd = process.cwd()
 
-const Mutex = require("async-mutex").Mutex
-const mutex = new Mutex()
 var shell = require("shelljs")
 
 var libCollector = require("./collector")
@@ -135,43 +133,42 @@ const prepareBranch = async function (
   if (error) return errorResult(stderr)
 }
 
-function benchBranch(app, config) {
+async function benchBranch(app, config) {
   app.log("Waiting our turn to run benchBranch...")
 
-  return mutex.runExclusive(async function () {
-    try {
-      if (config.repo != "substrate") {
-        return errorResult("Node benchmarks only available on Substrate.")
-      }
-
-      var id = config.id
-      var benchConfig = BenchConfigs[id]
-      if (!benchConfig) {
-        return errorResult(`Bench configuration for "${id}" was not found`)
-      }
-
-      const collector = new libCollector.Collector()
-      var benchContext = new BenchContext(app, config)
-      var { title, benchCommand } = benchConfig
-      app.log(`Started benchmark "${title}."`)
-
-      var error = await prepareBranch(config, { benchContext })
-      if (error) return error
-
-      var { stderr, error, stdout } = benchContext.runTask(
-        benchCommand,
-        `Benching branch ${config.branch}...`,
-      )
-      if (error) return errorResult(stderr)
-
-      await collector.CollectBranchCustomRunner(stdout)
-      let output = await collector.Report()
-
-      return { title, output, extraInfo: "", benchCommand }
-    } catch (error) {
-      return errorResult("Caught exception in benchBranch", error)
+  try {
+    if (config.repo != "substrate") {
+      return errorResult("Node benchmarks only available on Substrate.")
     }
-  })
+
+    var id = config.id
+    var benchConfig = BenchConfigs[id]
+    if (!benchConfig) {
+      return errorResult(`Bench configuration for "${id}" was not found`)
+    }
+
+    const collector = new libCollector.Collector()
+    var benchContext = new BenchContext(app, config)
+    var { title, benchCommand } = benchConfig
+    app.log(`Started benchmark "${title}."`)
+
+    var error = await prepareBranch(config, { benchContext })
+    if (error) return error
+
+    var { stderr, error, stdout } = benchContext.runTask(
+      benchCommand,
+      `Benching branch ${config.branch}...`,
+    )
+    if (error) return errorResult(stderr)
+
+    await collector.CollectBranchCustomRunner(stdout)
+    let output = await collector.Report()
+
+    return { title, output, extraInfo: "", benchCommand }
+  } catch (error) {
+    return errorResult("Caught exception in benchBranch", error)
+  }
+
 }
 
 var SubstrateRuntimeBenchmarkConfigs = {
@@ -444,168 +441,163 @@ function checkAllowedCharacters(command) {
   return true
 }
 
-function benchmarkRuntime(app, config) {
+async function benchmarkRuntime(app, config) {
   app.log("Waiting our turn to run benchmarkRuntime...")
 
-  return mutex.runExclusive(async function () {
-    try {
-      if (config.extra.split(" ").length < 2) {
-        return errorResult(`Incomplete command.`)
-      }
+  try {
+    if (config.extra.split(" ").length < 2) {
+      return errorResult(`Incomplete command.`)
+    }
 
-      let command = config.extra.split(" ")[0]
+    let command = config.extra.split(" ")[0]
 
-      var benchConfig
-      if (config.repo == "substrate" && config.id == "runtime") {
-        benchConfig = SubstrateRuntimeBenchmarkConfigs[command]
-      } else if (config.repo == "polkadot" && config.id == "runtime") {
-        benchConfig = PolkadotRuntimeBenchmarkConfigs[command]
-      } else if (config.repo == "polkadot" && config.id == "xcm") {
-        benchConfig = PolkadotXcmBenchmarkConfigs[command]
-      } else {
-        return errorResult(
-          `${config.repo} repo with ${config.id} is not supported.`,
-        )
-      }
-
-      var extra = config.extra.split(" ").slice(1).join(" ").trim()
-
-      if (!checkAllowedCharacters(extra)) {
-        return errorResult(`Not allowed to use #&|; in the command!`)
-      }
-
-      // Append extra flags to the end of the command
-      let benchCommand = benchConfig.benchCommand
-      if (command == "custom") {
-        // extra here should just be raw arguments to add to the command
-        benchCommand += " " + extra
-      } else {
-        // extra here should be the name of a pallet
-        benchCommand = benchCommand.replace("{pallet_name}", extra)
-        // custom output file name so that pallets with path don't cause issues
-        let outputFile = extra.includes("::")
-          ? extra.replace("::", "_") + ".rs"
-          : ""
-        benchCommand = benchCommand.replace("{output_file}", outputFile)
-        // pallet folder should be just the name of the pallet, without the leading
-        // "pallet_" or "frame_", then separated with "-"
-        let palletFolder = extra.split("_").slice(1).join("-").trim()
-        benchCommand = benchCommand.replace("{pallet_folder}", palletFolder)
-      }
-
-      let missing = checkRuntimeBenchmarkCommand(benchCommand)
-      if (missing.length > 0) {
-        return errorResult(`Missing required flags: ${missing.toString()}`)
-      }
-
-      var benchContext = new BenchContext(app, config)
-      var { title } = benchConfig
-      app.log(
-        `Started ${config.id} benchmark "${title}." (command: ${benchCommand})`,
+    var benchConfig
+    if (config.repo == "substrate" && config.id == "runtime") {
+      benchConfig = SubstrateRuntimeBenchmarkConfigs[command]
+    } else if (config.repo == "polkadot" && config.id == "runtime") {
+      benchConfig = PolkadotRuntimeBenchmarkConfigs[command]
+    } else if (config.repo == "polkadot" && config.id == "xcm") {
+      benchConfig = PolkadotXcmBenchmarkConfigs[command]
+    } else {
+      return errorResult(
+        `${config.repo} repo with ${config.id} is not supported.`,
       )
+    }
 
-      var error = await prepareBranch(config, { benchContext })
-      if (error) return error
+    var extra = config.extra.split(" ").slice(1).join(" ").trim()
 
-      const outputFile = benchCommand.match(/--output(?:=|\s+)(".+?"|\S+)/)[1]
-      var { stdout, stderr } = benchContext.runTask(
-        benchCommand,
-        `Running for branch ${config.branch}, ${outputFile ? `outputFile: ${outputFile}` : ""
-        }: ${benchCommand}`,
-      )
-      let extraInfo = ""
+    if (!checkAllowedCharacters(extra)) {
+      return errorResult(`Not allowed to use #&|; in the command!`)
+    }
 
-      var { stdout: gitStatus, stderr: gitStatusError } =
-        benchContext.runTask("git status --short")
-      app.log(`Git status after execution: ${gitStatus || gitStatusError}`)
+    // Append extra flags to the end of the command
+    let benchCommand = benchConfig.benchCommand
+    if (command == "custom") {
+      // extra here should just be raw arguments to add to the command
+      benchCommand += " " + extra
+    } else {
+      // extra here should be the name of a pallet
+      benchCommand = benchCommand.replace("{pallet_name}", extra)
+      // custom output file name so that pallets with path don't cause issues
+      let outputFile = extra.includes("::")
+        ? extra.replace("::", "_") + ".rs"
+        : ""
+      benchCommand = benchCommand.replace("{output_file}", outputFile)
+      // pallet folder should be just the name of the pallet, without the leading
+      // "pallet_" or "frame_", then separated with "-"
+      let palletFolder = extra.split("_").slice(1).join("-").trim()
+      benchCommand = benchCommand.replace("{pallet_folder}", palletFolder)
+    }
 
-      if (outputFile) {
-        if (process.env.DEBUG) {
-          app.log({
-            context: "Output file",
-            msg: fs.readFileSync(outputFile).toString(),
-          })
-        } else {
-          try {
+    let missing = checkRuntimeBenchmarkCommand(benchCommand)
+    if (missing.length > 0) {
+      return errorResult(`Missing required flags: ${missing.toString()}`)
+    }
+
+    var benchContext = new BenchContext(app, config)
+    var { title } = benchConfig
+    app.log(
+      `Started ${config.id} benchmark "${title}." (command: ${benchCommand})`,
+    )
+
+    var error = await prepareBranch(config, { benchContext })
+    if (error) return error
+
+    const outputFile = benchCommand.match(/--output(?:=|\s+)(".+?"|\S+)/)[1]
+    var { stdout, stderr } = benchContext.runTask(
+      benchCommand,
+      `Running for branch ${config.branch}, ${outputFile ? `outputFile: ${outputFile}` : ""
+      }: ${benchCommand}`,
+    )
+    let extraInfo = ""
+
+    var { stdout: gitStatus, stderr: gitStatusError } =
+      benchContext.runTask("git status --short")
+    app.log(`Git status after execution: ${gitStatus || gitStatusError}`)
+
+    if (outputFile) {
+      if (process.env.DEBUG) {
+        app.log({
+          context: "Output file",
+          msg: fs.readFileSync(outputFile).toString(),
+        })
+      } else {
+        try {
+          var last = benchContext.runTask(
+            `git add ${outputFile} && git commit -m "${benchCommand}"`,
+          )
+          if (last.error) {
+            extraInfo = `ERROR: Unable to commit file ${outputFile}`
+            config.logFatal({
+              msg: extraInfo,
+              stdout: last.stdout,
+              stderr: last.stderr,
+            })
+          } else {
+            const target = `${config.contributor}/${config.repo}`
+            const { url, token } = await config.getPushDomain()
             var last = benchContext.runTask(
-              `git add ${outputFile} && git commit -m "${benchCommand}"`,
+              `git remote set-url pr ${url}/${target}.git && git push pr HEAD`,
+              `Pushing ${outputFile} to ${config.branch}`,
             )
             if (last.error) {
-              extraInfo = `ERROR: Unable to commit file ${outputFile}`
+              extraInfo = `ERROR: Unable to push ${outputFile}`
               config.logFatal({
                 msg: extraInfo,
                 stdout: last.stdout,
                 stderr: last.stderr,
               })
-            } else {
-              const target = `${config.contributor}/${config.repo}`
-              const { url, token } = await config.getPushDomain()
-              var last = benchContext.runTask(
-                `git remote set-url pr ${url}/${target}.git && git push pr HEAD`,
-                `Pushing ${outputFile} to ${config.branch}`,
-              )
-              if (last.error) {
-                extraInfo = `ERROR: Unable to push ${outputFile}`
-                config.logFatal({
-                  msg: extraInfo,
-                  stdout: last.stdout,
-                  stderr: last.stderr,
-                })
-              }
             }
-          } catch (error) {
-            extraInfo =
-              "NOTE: Caught exception while trying to push commits to the repository"
-            config.logFatal({ msg: extraInfo, error })
           }
+        } catch (error) {
+          extraInfo =
+            "NOTE: Caught exception while trying to push commits to the repository"
+          config.logFatal({ msg: extraInfo, error })
         }
       }
-
-      return {
-        title,
-        output: stdout ? stdout : stderr,
-        extraInfo,
-        benchCommand,
-      }
-    } catch (error) {
-      return errorResult("Caught exception in benchmarkRuntime", error)
     }
-  })
+
+    return {
+      title,
+      output: stdout ? stdout : stderr,
+      extraInfo,
+      benchCommand,
+    }
+  } catch (error) {
+    return errorResult("Caught exception in benchmarkRuntime", error)
+  }
 }
 
-function benchRustup(app, config) {
+async function benchRustup(app, config) {
   app.log("Waiting our turn to run benchRustup...")
 
-  return mutex.runExclusive(async function () {
-    try {
-
-      // right now only `rustup update` is supported.
-      if (config.extra != "update") {
-        return errorResult(`Invalid "rustup" command. Only "update" is supported.`)
-      }
-
-      const collector = new libCollector.Collector()
-      var benchContext = new BenchContext(app, config)
-
-      let benchCommand = "rustup update";
-      let title = "Rustup Update";
-
-      var { stderr, error, stdout } = benchContext.runTask(
-        benchCommand,
-        `Executing "rustup update"...`,
-      )
-      if (error) return errorResult(stderr)
-
-      return {
-        title,
-        output: stdout ? stdout : stderr,
-        extraInfo: "",
-        benchCommand
-      }
-    } catch (error) {
-      return errorResult("Caught exception in benchRustup", error)
+  try {
+    // right now only `rustup update` is supported.
+    if (config.extra != "update") {
+      return errorResult(`Invalid "rustup" command. Only "update" is supported.`)
     }
-  })
+
+    const collector = new libCollector.Collector()
+    var benchContext = new BenchContext(app, config)
+
+    let benchCommand = "rustup update";
+    let title = "Rustup Update";
+
+    var { stderr, error, stdout } = benchContext.runTask(
+      benchCommand,
+      `Executing "rustup update"...`,
+    )
+    if (error) return errorResult(stderr)
+
+    return {
+      title,
+      output: stdout ? stdout : stderr,
+      extraInfo: "",
+      benchCommand
+    }
+  } catch (error) {
+    return errorResult("Caught exception in benchRustup", error)
+  }
 }
 
 module.exports = {
