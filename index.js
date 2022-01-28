8b7e1baed721b30e2118f7325ff334e5f201adc1
joao-paulo-parity <joao-paulo@parity.io>
2022-01-28 14:39:13 -0300

    move mutex to event handlers
    so that we only restart the app after the current event finishes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { createAppAuth } = require("@octokit/auth-app")
 const assert = require("assert")
 const fs = require("fs")
 const shell = require("shelljs")
+const Mutex = require("async-mutex").Mutex
 
 var { benchBranch, benchmarkRuntime, benchRustup } = require("./bench")
 
@@ -10,6 +11,8 @@ const githubCommentLimitTruncateMessage = "<truncated>..."
 
 let isTerminating = false
 let logFatal = undefined
+
+let pendingPayloadCount = 0
 
 for (const event of ["uncaughtException", "unhandledRejection"]) {
   process.on(event, function (error, origin) {
@@ -30,6 +33,7 @@ for (const event of ["uncaughtException", "unhandledRejection"]) {
   })
 }
 
+const mutex = new Mutex()
 module.exports = (app) => {
   if (process.env.DEBUG) {
     app.log("Running in debug mode")
@@ -44,16 +48,27 @@ module.exports = (app) => {
   // FIXME: This is suboptimal and we should not have to stop the application in
   // case of errors
   // The server will automatically restarted on failures in ./run
+  const exitWhenFree = async function() {
+    const isFree = await mutex.runExclusive(function () {
+      return pendingPayloadCount === 0
+    })
+    if (isFree) {
+      process.exit(1)
+    } else {
+      await exitWhenFree()
+    }
+  }
+  const logThenExit = function(log) {
+    return async function(...args) {
+      log(...args)
+      // only exit the application after the current events have been processed
+      await exitWhenFree()
+    }
+  }
   const logError = app.log.error
-  app.log.error = function(...args) {
-    logError(...args)
-    process.exit(1)
-  }
+  app.log.error = logThenExit(logError)
   logFatal = app.log.fatal
-  app.log.fatal = function(...args) {
-    logFatal(...args)
-    process.exit(1)
-  }
+  app.log.fatal = logThenExit(logFatal)
 
   const baseBranch = process.env.BASE_BRANCH || "master"
   app.log.debug(`base branch: ${baseBranch}`)
@@ -79,179 +94,183 @@ module.exports = (app) => {
   })
 
   app.on("issue_comment", async (context) => {
-    let commentText = context.payload.comment.body
-    if (
-      !context.payload.issue.hasOwnProperty("pull_request") ||
-      context.payload.action !== "created" ||
-      !commentText.startsWith("/bench")
-    ) {
-      return
-    }
-
-    try {
-      const installationId = (context.payload.installation || {}).id
-      if (!installationId) {
-        await context.octokit.issues.createComment(
-          context.issue({
-            body: `Error: Installation id was missing from webhook payload`,
-          }),
-        )
+    pendingPayloadCount++
+    await mutex.runExclusive(async function() {
+      let commentText = context.payload.comment.body
+      if (
+        !context.payload.issue.hasOwnProperty("pull_request") ||
+        context.payload.action !== "created" ||
+        !commentText.startsWith("/bench")
+      ) {
         return
       }
 
-      const getPushDomain = async function () {
-        const token = (
-          await authInstallation({ type: "installation", installationId })
-        ).token
-
-        const url = `https://x-access-token:${token}@github.com`
-        return { url, token }
-      }
-
-      const repo = context.payload.repository.name
-      const owner = context.payload.repository.owner.login
-      const pull_number = context.payload.issue.number
-
-      // Capture `<action>` in `/bench <action> <extra>`
-      let action = commentText.split(" ").splice(1, 1).join(" ").trim()
-      // Capture all `<extra>` text in `/bench <action> <extra>`
-      let extra = commentText.split(" ").splice(2).join(" ").trim()
-
-      let pr = await context.octokit.pulls.get({ owner, repo, pull_number })
-      const contributor = pr.data.head.user.login
-      const branch = pr.data.head.ref
-      app.log.debug(`branch: ${branch}`)
-
-      var { stdout: toolchain, code: toolchainError } = shell.exec(
-        "rustup show active-toolchain --verbose",
-        { silent: false },
-      )
-      if (toolchainError) {
-        await context.octokit.issues.createComment(
-          context.issue({
-            body: "ERROR: Failed to query the currently active Rust toolchain",
-          }),
-        )
-        return
-      } else {
-        toolchain = toolchain.trim()
-      }
-
-      const initialInfo = `Starting benchmark for branch: ${branch} (vs ${baseBranch})\n\nToolchain: \n${toolchain}\n\n Comment will be updated.`
-      let comment_id = undefined
-      if (process.env.DEBUG) {
-        app.log(initialInfo)
-      } else {
-        const issueComment = context.issue({ body: initialInfo })
-        const issue_comment = await context.octokit.issues.createComment(
-          issueComment,
-        )
-        comment_id = issue_comment.data.id
-      }
-
-      let config = {
-        owner,
-        contributor,
-        repo,
-        branch,
-        baseBranch,
-        id: action,
-        extra,
-        getPushDomain,
-        logFatal
-      }
-
-      let report
-      if (action == "runtime" || action == "xcm") {
-        report = await benchmarkRuntime(app, config)
-      } else if (action == "rustup") {
-        report = await benchRustup(app, config)
-      } else {
-        report = await benchBranch(app, config)
-      }
-      if (process.env.DEBUG) {
-        console.log(report)
-        return
-      }
-
-      if (report.isError) {
-        logError(report.message)
-
-        if (report.error) {
-          logError(report.error)
+      try {
+        const installationId = (context.payload.installation || {}).id
+        if (!installationId) {
+          await context.octokit.issues.createComment(
+            context.issue({
+              body: `Error: Installation id was missing from webhook payload`,
+            }),
+          )
+          return
         }
 
-        const output = `${report.message}${report.error ? `: ${report.error.toString()}` : ""
-          }`
+        const getPushDomain = async function () {
+          const token = (
+            await authInstallation({ type: "installation", installationId })
+          ).token
+
+          const url = `https://x-access-token:${token}@github.com`
+          return { url, token }
+        }
+
+        const repo = context.payload.repository.name
+        const owner = context.payload.repository.owner.login
+        const pull_number = context.payload.issue.number
+
+        // Capture `<action>` in `/bench <action> <extra>`
+        let action = commentText.split(" ").splice(1, 1).join(" ").trim()
+        // Capture all `<extra>` text in `/bench <action> <extra>`
+        let extra = commentText.split(" ").splice(2).join(" ").trim()
+
+        let pr = await context.octokit.pulls.get({ owner, repo, pull_number })
+        const contributor = pr.data.head.user.login
+        const branch = pr.data.head.ref
+        app.log.debug(`branch: ${branch}`)
+
+        var { stdout: toolchain, code: toolchainError } = shell.exec(
+          "rustup show active-toolchain --verbose",
+          { silent: false },
+        )
+        if (toolchainError) {
+          await context.octokit.issues.createComment(
+            context.issue({
+              body: "ERROR: Failed to query the currently active Rust toolchain",
+            }),
+          )
+          return
+        } else {
+          toolchain = toolchain.trim()
+        }
+
+        const initialInfo = `Starting benchmark for branch: ${branch} (vs ${baseBranch})\n\nToolchain: \n${toolchain}\n\n Comment will be updated.`
+        let comment_id = undefined
+        if (process.env.DEBUG) {
+          app.log(initialInfo)
+        } else {
+          const issueComment = context.issue({ body: initialInfo })
+          const issue_comment = await context.octokit.issues.createComment(
+            issueComment,
+          )
+          comment_id = issue_comment.data.id
+        }
+
+        let config = {
+          owner,
+          contributor,
+          repo,
+          branch,
+          baseBranch,
+          id: action,
+          extra,
+          getPushDomain,
+          logFatal
+        }
+
+        let report
+        if (action == "runtime" || action == "xcm") {
+          report = await benchmarkRuntime(app, config)
+        } else if (action == "rustup") {
+          report = await benchRustup(app, config)
+        } else {
+          report = await benchBranch(app, config)
+        }
+        if (process.env.DEBUG) {
+          console.log(report)
+          return
+        }
+
+        if (report.isError) {
+          logError(report.message)
+
+          if (report.error) {
+            logError(report.error)
+          }
+
+          const output = `${report.message}${report.error ? `: ${report.error.toString()}` : ""
+            }`
+
+          await context.octokit.issues.updateComment({
+            owner,
+            repo,
+            comment_id,
+            body: `Error running benchmark: **${branch}**\n\n<details><summary>stdout</summary>${output}</details>`,
+          })
+
+          return
+        }
+
+        let { title, output, extraInfo, benchCommand } = report
+
+        const bodyPrefix = `
+  Benchmark **${title}** for branch "${branch}" with command ${benchCommand}
+
+  Toolchain: ${toolchain}
+
+  <details>
+  <summary>Results</summary>
+
+  \`\`\`
+  `.trim()
+
+        const bodySuffix = `
+  \`\`\`
+
+  </details>
+  `.trim()
+
+        const padding = 16
+        const formattingLength =
+          bodyPrefix.length + bodySuffix.length + extraInfo.length + padding
+        const length = formattingLength + output.length
+        if (length >= githubCommentLimitLength) {
+          output = `${output.slice(
+            0,
+            githubCommentLimitLength -
+            (githubCommentLimitTruncateMessage.length + formattingLength),
+          )}${githubCommentLimitTruncateMessage}`
+        }
+
+        const body = `
+  ${bodyPrefix}
+  ${output}
+  ${bodySuffix}
+
+  ${extraInfo}
+  `.trim()
 
         await context.octokit.issues.updateComment({
           owner,
           repo,
           comment_id,
-          body: `Error running benchmark: **${branch}**\n\n<details><summary>stdout</summary>${output}</details>`,
+          body,
         })
-
-        return
+      } catch (error) {
+        logFatal({
+          error,
+          repo,
+          owner,
+          pull_number,
+          msg: "Caught exception in issue_comment's handler",
+        })
+        await context.octokit.issues.createComment(
+          context.issue({
+            body: `Exception caught: \`${error.message}\`\n${error.stack}`,
+          }),
+        )
       }
-
-      let { title, output, extraInfo, benchCommand } = report
-
-      const bodyPrefix = `
-Benchmark **${title}** for branch "${branch}" with command ${benchCommand}
-
-Toolchain: ${toolchain}
-
-<details>
-<summary>Results</summary>
-
-\`\`\`
-`.trim()
-
-      const bodySuffix = `
-\`\`\`
-
-</details>
-`.trim()
-
-      const padding = 16
-      const formattingLength =
-        bodyPrefix.length + bodySuffix.length + extraInfo.length + padding
-      const length = formattingLength + output.length
-      if (length >= githubCommentLimitLength) {
-        output = `${output.slice(
-          0,
-          githubCommentLimitLength -
-          (githubCommentLimitTruncateMessage.length + formattingLength),
-        )}${githubCommentLimitTruncateMessage}`
-      }
-
-      const body = `
-${bodyPrefix}
-${output}
-${bodySuffix}
-
-${extraInfo}
-`.trim()
-
-      await context.octokit.issues.updateComment({
-        owner,
-        repo,
-        comment_id,
-        body,
-      })
-    } catch (error) {
-      logFatal({
-        error,
-        repo,
-        owner,
-        pull_number,
-        msg: "Caught exception in issue_comment's handler",
-      })
-      await context.octokit.issues.createComment(
-        context.issue({
-          body: `Exception caught: \`${error.message}\`\n${error.stack}`,
-        }),
-      )
-    }
+    })
+    pendingPayloadCount--
   })
 }
